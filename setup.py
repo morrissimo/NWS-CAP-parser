821865fbc9e1194047a0529d8553e2e24049f473
Robert Morris <robert.f.morris@gmail.com>
2012-05-09 15:25:37 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import glob
+
+setup(name='nwscapparser',
+      version='1.0',
+      description='NWS CAP Parser',
+      author='Robert Morris',
+      author_email='robert@emthree.com',
+      url='https://github.com/morrissimo/NWS-CAP-parser/',
+      packages=['nwscapparser'],
+      data_files=[('',glob.glob('*.xml')),
+        ('',['demo.py','readme.md','LICENSE'])]
+     )
